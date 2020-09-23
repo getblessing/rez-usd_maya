bca81a29e5efc33d63795937c37d666d87dcf099
David Lai <davidlatwe@gmail.com>
2020-09-23 14:04:03 +0800

    update

--- rezbuild.py
+++ rezbuild.py
@@ -12,7 +12,7 @@ IS_MAC = platform.system() == "Darwin"
 
 
 url_prefix = "https://github.com/Autodesk/maya-usd/archive"
-filename = "v0.1.0.zip"
+filename = "v0.3.0.zip"
 
 
 def shorten_variant_build_path(source_path):
@@ -47,13 +47,41 @@ def build(source_path, build_path, install_path, targets):
     # Unzip the source
     source_root = lib.open_archive(archive)
 
+    if is_debug:
+        # For bypassing "fatal error LNK1104: cannot open 'python27_d.lib'"
+        #
+        # Although there's a flag `MAYAUSD_DEFINE_BOOST_DEBUG_PYTHON_FLAG`
+        # for not using python debug, which will set `Boost_USE_DEBUG_PYTHON`
+        # to `ON` when `MAYAUSD_DEFINE_BOOST_DEBUG_PYTHON_FLAG=ON`, but the
+        # following settings still try to use python debug lib even it's OFF.
+        #
+        # Not sure what these patch will affect, but I *guess* it should be
+        # safe..
+        #
+        cmake_lists = [
+            "/lib/usd/utils/CMakeLists.txt",
+            "/lib/usd/schemas/CMakeLists.txt",
+            "/lib/mayaUsd/CMakeLists.txt",
+            "/plugin/al/schemas/AL/usd/schemas/maya/CMakeLists.txt",
+            "/plugin/al/schemas/AL/usd/schemas/mayatest/CMakeLists.txt",
+            "/plugin/al/usdmayautils/AL/usdmaya/utils/CMakeLists.txt",
+            "/plugin/al/usdtransaction/AL/usd/transaction/CMakeLists.txt",
+            "/plugin/al/usdtransaction/AL/usd/transaction/tests/CMakeLists.txt",
+        ]
+        for fname in cmake_lists:
+            lib.patch_file(
+                source_root + fname,
+                [("$<$<STREQUAL:${CMAKE_BUILD_TYPE},Debug>:BOOST_DEBUG_PYTHON>",
+                  "# $<$<STREQUAL:${CMAKE_BUILD_TYPE},Debug>:BOOST_DEBUG_PYTHON>")]
+            )
+
     # Build
     with lib.working_dir(build_path + "/_usd_maya"):
         extra_args = [
             "-DMAYAUSD_DEFINE_BOOST_DEBUG_PYTHON_FLAG=OFF",
 
             "-DBUILD_SHARED_LIBS=ON",
-
+            "-DBUILD_WITH_PYTHON_3=OFF",
             "-DPython_EXECUTABLE=\"%s\"" % py_info[0].replace("\\", "/"),
             "-DPYTHON_LIBRARIES=\"%s\"" % py_info[1].replace("\\", "/"),
             "-DPYTHON_INCLUDE_DIRS=\"%s\"" % py_info[2].replace("\\", "/"),
@@ -66,10 +94,15 @@ def build(source_path, build_path, install_path, targets):
             "-DCMAKE_WANT_UFE_BUILD=ON",
 
             "-DBUILD_TESTS=ON",
-            "-DPXR_STRICT_BUILD_MODE=OFF",  # warning as error
+            "-DBUILD_STRICT_MODE=OFF",  # warning as error
+            # If `BUILD_STRICT` enabled, may get encode warnings e.g. in
+            #   `include\pxr/imaging/hd/renderIndex.h`, and build failed.
 
             "-DQT_LOCATION=\"%s\"" % os.environ["QT_LOCATION"],
         ]
+        if IS_WIN:
+            # Increase the precompiled header buffer limit.
+            extra_args.append("-DCMAKE_CXX_FLAGS=\"/Zm150\"")
 
         lib.run_cmake(source_root, install_path, extra_args=extra_args)
 
